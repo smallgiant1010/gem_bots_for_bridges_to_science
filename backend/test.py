d629103dd95bb0787994bee54a8c1f2e5e145a9d
smallgiant1010 <smallgiant1010@gmail.com>
2025-04-30 18:25:05 -0400

    removed testing file

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def main():
-    uptimeRobotIPAddressRequest = requests.get("https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt").text.split("\n")
-    print(uptimeRobotIPAddressRequest)
-
-
-if __name__ == "__main__":
-    main()
